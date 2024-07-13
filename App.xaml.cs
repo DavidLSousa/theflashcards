072421e32736e4d3f324b19b152a2c71d37be08a
David Sousa <davidlsousa.dev@gmail.com>
2024-07-13 14:54:20 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace theflashcards
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
