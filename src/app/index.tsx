b18c629e2c28f98a093cd3bb811f132f52e82e2b
David Sousa <davidlsousa.dev@gmail.com>
2025-04-29 18:01:40 -0300

    feat: add background

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,18 @@
 import { View, Text, StyleSheet } from "react-native";
 import Header from "@/src/components/header";
+import { colors } from "../constants/colors";
 
 export default function Index() {
   return (
-    <Header />
+    <View style={styles.container}>
+      <Header />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.whiteIce,
+  },
+})
