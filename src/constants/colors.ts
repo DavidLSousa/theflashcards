d85faaefbe5814152d090035d0d57ac834af113e
David Sousa <davidlsousa.dev@gmail.com>
2025-04-29 18:02:06 -0300

    create colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+
+export const colors = {
+  text: '#333',
+  success: '#22a258',
+  error: '#be4032',
+
+  whiteIce: '#F0F0F0',
+  platinum: '#E6E6E6',
+  blueLazuli: '#47688D',
+  blueMedium: '#364F6B',
+  blueDark: '#253649',
+};
