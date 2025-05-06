f146945c9c744b8d6dc927ed437f5c5e0284ce09
David Sousa <davidlsousa.dev@gmail.com>
2025-05-06 09:36:23 -0300

    fix: ajust header

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -3,12 +3,11 @@ import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: "center",
-    gap: 10,
+    marginBottom: 10,
   },
   title: {
-    fontSize: 24,
+    fontSize: 36,
     fontWeight: "bold",
     color: colors.whiteIce,
     marginTop: 10,
