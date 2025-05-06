76f797489446cc16929a9345b3a1906380d3274f
David Sousa <davidlsousa.dev@gmail.com>
2025-05-06 09:36:57 -0300

    feat: add buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import Header from "@/src/components/header";
 import { colors } from "../constants/colors";
+import ButtonHome from "@/src/components/buttonHome";
 
 export default function Index() {
   return (
@@ -11,6 +12,16 @@ export default function Index() {
       style={styles.container}
     >
       <Header />
+      
+
+      <View style={  styles.buttonsContainer }>
+        <ButtonHome title="Criar Flash Card" />
+        <ButtonHome title="Todos Flash Cards" />
+        <ButtonHome title="Crie seu Simulado" />
+        <ButtonHome title="Traga seus cards" />
+        <ButtonHome title="Traga seus cards" />
+      </View>
+    
     </LinearGradient>
   );
 }
@@ -20,4 +31,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.whiteIce,
   },
+  button: {
+    backgroundColor: colors.whiteIce,
+  },
+  buttonsContainer: {
+    flex: 1,
+    justifyContent: "center",
+  }
 })
