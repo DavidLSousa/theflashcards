a7be2f865d52ab09d764018552d463da6d23a2e9
David Sousa <davidlsousa.dev@gmail.com>
2025-05-19 21:56:38 -0300

    feat newCardPage

--- src/app/newCardPage.tsx
+++ src/app/newCardPage.tsx
@@ -1,15 +1,73 @@
 import { LinearGradient } from "expo-linear-gradient";
-import Header from "@/src/components/header";
+import { useState } from "react";
+import { TextInput, TouchableOpacity, View, Text, StyleSheet } from "react-native";
+
 import { colors } from "@/src/constants/colors";
-import { styles } from "./styles";
+import Header from "@/src/components/header";
+import Input from "@/src/components/input";
+import Button from "@/src/components/button";
 
 export default function NewCardPage() {
+  const [quest, setQuest] = useState("");
+  const [resp, setResp] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleCreateCard = () => {
+    console.log("Novo card:", { category, quest, resp });
+    
+    // Logic
+
+    setQuest("");
+    setResp("");
+  };
+
   return (
-    <LinearGradient 
-          colors={[colors.blueLazuli, colors.blueMedium]}
-          style={styles.container}
-        >
-          <Header />
+    <LinearGradient
+      colors={[colors.blueLazuli, colors.blueMedium]}
+      style={styles.container}
+    >
+      <Header />
+
+      <View style={styles.form}>
+
+        <Input
+          placeholder="Pergunta"
+          value={quest}
+          onChangeText={setQuest}
+          multiline
+          numberOfLines={4}
+        />
+        <Input
+          placeholder="Resposta"
+          value={resp}
+          onChangeText={setResp}
+          multiline
+          numberOfLines={4}
+        />
+        <Input
+          placeholder="Categoria"
+          value={category}
+          onChangeText={setCategory}
+        />
+
+        <Button
+          title="Criar Flash Card"
+          onPress={handleCreateCard}
+        />
+      </View>
+
     </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 60,
+  },
+  form: {
+    marginTop: 30,
+    gap: 16,
+  }
+});
