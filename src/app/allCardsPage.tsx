3a613e418678301ac56e448215922d2e9d187de6
David Sousa <davidlsousa.dev@gmail.com>
2025-05-22 22:29:18 -0300

    implementing base allcardspage

--- src/app/allCardsPage.tsx
+++ src/app/allCardsPage.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import { ScrollView } from "react-native";
+
 import Header from "@/src/components/header";
+import Card from "@/src/components/card";
 import { colors } from "@/src/constants/colors";
 import { styles } from "./styles";
 
 export default function AllCardsPage() {
+  const [showAnswer, setShowAnswer] = useState(false);
+
   return (
-    <LinearGradient 
-          colors={[colors.blueLazuli, colors.blueMedium]}
-          style={styles.container}
-        >
-          <Header />
+    <LinearGradient
+      colors={[colors.blueLazuli, colors.blueMedium]}
+      style={styles.container}
+    >
+      <Header />
+
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        
+        <Card showAnswer={showAnswer} setShowAnswer={setShowAnswer} />
+        
+      </ScrollView>
     </LinearGradient>
   );
 }
