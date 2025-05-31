febb621adf712c2a9577a522d811dde6f155f98c
David Sousa <davidlsousa.dev@gmail.com>
2025-05-31 00:03:10 -0300

    create category repository

---
+++ src/repositories/categoryRepository.ts
@@ -0,0 +1,10 @@
+import * as FileSystem from 'expo-file-system';
+
+import { Card } from '@/src/models/Card';
+
+export class CardRepository {
+  private path = `${FileSystem.documentDirectory}categories.json`;
+
+
+
+}
